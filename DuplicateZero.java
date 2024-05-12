11f18061a6faaded63466102259f5c7f26261ba9
saswata sahu <saswata2sahu@gmail.com>
2024-05-12 10:09:32 +0530

    resolved duplicate zero error

--- DuplicateZero.java
+++ DuplicateZero.java
@@ -2,12 +2,12 @@ public class DuplicateZero {
     public static void main(String[] args) {
         int arr[]={1,0,2,4,5,6,0,8};
         int i=0;
-        while(i<arr.length){
+        while(i<arr.length-1){
              if(arr[i]==0){
-                for(int j=arr.length-1;j>=i+2;j--){
+                for(int j=arr.length-1;j>=i+1;j--){
                     arr[j]=arr[j-1];
                 }
-                arr[i+1]=0;
+                
                 i+=2;
             }else{
                  i++;
